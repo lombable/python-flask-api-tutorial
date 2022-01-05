ac8149b158f79d40890d8c53ed78a239957dbd9c
lombable <rinconaedgar@gmail.com>
2022-01-05 23:01:39 +0000

    first commit

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    jsonify(todos)
+    return todos
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
